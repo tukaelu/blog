49109e7313b3e73acca81ddf7b62a1798aa1faf7
tuka <tsukasa@birdcage.jp>
2019-12-31 07:01:27 +0900

    improve emoji support

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,30 @@ module.exports = {
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
           `gatsby-remark-external-links`,
-          `gatsby-remark-emoji-unicode`,
+          {
+            resolve: 'gatsby-remark-emojis',
+            options: {
+              // Deactivate the plugin globally (default: true)
+              active : true,
+              // Add a custom css class
+              class  : 'emoji-icon',
+              // In order to avoid pattern mismatch you can specify
+              // an escape character which will be prepended to the
+              // actual pattern (e.g. `#:poop:`).
+              // escapeCharacter : '#', // (default: '')
+              // Select the size (available size: 16, 24, 32, 64)
+              size   : 64,
+              // Add custom styles
+              styles : {
+                display      : 'inline',
+                margin       : '0',
+                'margin-top' : '1px',
+                position     : 'relative',
+                top          : '5px',
+                width        : '25px'
+              }
+            }
+          },
         ],
       },
     },
